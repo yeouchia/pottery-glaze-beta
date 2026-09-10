4294b0783d4e747caeed0607ba4fd615ee73029d
yeouchia <yeouchia@gmail.com>
2026-09-10 18:51:43 +0800

    Update knowledge-data.js

--- assets/js/knowledge-data.js
+++ assets/js/knowledge-data.js
@@ -34,7 +34,8 @@ const knowledgeTree = [
         articles: [
             { id: "glaze_basics-1", title: "何謂釉藥、釉的本質？", url: "./book/glaze_basics/glaze_basics-1.html" },
             { id: "glaze_basics-2", title: "認識釉藥三大元素", url: "./book/glaze_basics/glaze_basics-2.html" },
-            { id: "glaze_basics-1-3", title: "釉的種類？", url: "./book/glaze_basics/glaze_basics-1-3.html" }
+            { id: "glaze_basics-3", title: "釉的種類？", url: "./book/glaze_basics/glaze_basics-3.html" },
+            { id: "glaze_basics-4", title: "玻璃質如何結合在陶瓷？揭開釉藥燒熔的奧秘？", url: "./book/glaze_basics/glaze_basics-4.html" }
         ]
     },
     {
